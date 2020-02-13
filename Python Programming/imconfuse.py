96eb142bda04b0fe6e27c3f7f3856496d3381e95
Ahmed Jadani <ahmed4star@gmail.com>
2020-02-13 23:41:45 +0100

    Update imconfuse.py
    Edited

--- Python Programming/imconfuse.py
+++ Python Programming/imconfuse.py
@@ -4,3 +4,5 @@
 print (0.1 + 0.2 == 0.3)
 #vs
 print (0.1 + 0.2 == 0.30000000000000004)
+
+##edit: not just with python, almost all programming language i've learned, i believe this could be a deadly bug.
