86b7efee76fda7af1efc5ada452cb89e627570aa
Ahmed Jadani <ahmed4star@gmail.com>
2020-02-14 14:15:17 +0100

    Learn :: Heskell -> FunctionalProgramming -> FunctionalPrograms

---
+++ Haskell Programming/EasyFactorial.hs
@@ -0,0 +1,9 @@
+{-||                      ||-}
+{-||   Learning Haskell   ||-}
+{-||                      ||-}
+{-||  Author: Ahmed0x7ce  ||-}
+
+import Data.List
+import System.IO
+
+easyFact n = product [1..n]
