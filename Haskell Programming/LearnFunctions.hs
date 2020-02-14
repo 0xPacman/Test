c285fabf0e7278a0a36c023164f2f9f9eccc638c
Ahmed Jadani <ahmed4star@gmail.com>
2020-02-14 13:50:49 +0100

    Learn :: Heskell -> FunctionalProgramming -> FunctionalPrograms

---
+++ Haskell Programming/LearnFunctions.hs
@@ -0,0 +1,27 @@
+{-||                      ||-}
+{-||   Learning Haskell   ||-}
+{-||                      ||-}
+{-||  Author: Ahmed0x7ce  ||-}
+
+-- Hello!
+{- let's learn some haskell -}
+
+import Data.List
+import System.IO
+import Prelude
+
+-- Functions
+{-↙️ (1)    ⬇️ (2)  ⬇️ (3)  ⬇ (4) -}
+addnums :: Int -> Int -> Int
+
+{-
+(1) = Function name
+(2) + (3) = Parameters
+(4) = Return value
+-}
+
+addnums x y = x+y
+
+-- On the console type ":t addnums" to check the declaration
+-- The min intger is : -9223372036854775808
+-- The max intger is : 9223372036854775807
