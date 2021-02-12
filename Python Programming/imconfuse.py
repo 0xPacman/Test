6b98c46bda0269d434781f7428843c7141e4b946
Ahmed Jadani <ahmed4star@gmail.com>
2021-02-12 10:00:05 +0100

    Solved.

--- Python Programming/imconfuse.py
+++ Python Programming/imconfuse.py
@@ -6,3 +6,4 @@ print (0.1 + 0.2 == 0.3)
 print (0.1 + 0.2 == 0.30000000000000004)
 
 ##edit: not just with python, almost all programming language i've learned, i believe this could be a deadly bug.
+##edit 2: i found the answer here: https://0.30000000000000004.com/
