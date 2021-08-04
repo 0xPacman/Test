f5984b7ccf2934e535e6b258af62749536523d18
Ahmed Jadani <ahmed4star@gmail.com>
2021-08-04 23:38:51 +0200

    Update SimpleFacto.lisp

--- Lisp Programming/SimpleFacto.lisp
+++ Lisp Programming/SimpleFacto.lisp
@@ -1,7 +1,7 @@
 #||                      ||#
 #||    Learning Lisp     ||#
 #||                      ||#
-#||  Author: Ahmed0x7ce  ||#
+#||   Author: 0xPacman   ||#
 
 (defun fact (n)
   (if (or (= n 1) ( = n 0))
